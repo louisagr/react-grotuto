aabcac5455941ac61829e33b111e9273ad5b3e39
eleve <augereau.louis@gmail.com>
2018-05-09 16:10:46 +0200

    pre-commit, npm eject

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-
 class App extends Component {
   state = {
     persons: [
@@ -47,11 +46,13 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
       cursor: "pointer"
+
     };
 
     let persons = null;
@@ -70,17 +71,28 @@ class App extends Component {
 
       </div>
       );
+      style.backgroundColor = "red";
+    }
+
+    const classes = []; 
+    if (this.state.persons.length <= 2) {
+      classes.push("red"); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold"); // classes = ["red", "bold"]
     }
 
     return (
+
       <div className="App">
         <h1>Hi, I'm a React app</h1>
-        <p>This is really working</p>
+        <p className={classes.join(" ")}>This is really working</p>
         <button 
         style={style}
         onClick={this.togglePersonsHandler}>Toggle</button>
         {persons}
       </div>
+
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
